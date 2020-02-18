0525b1f7d78d825c5daee6043c0241edb7acbfba
ofbimon <bimon@alterra.id>
2020-02-18 12:54:52 +0700

    Updates

---
+++ precache-manifest.ab0cda3a21740861fbd4a0909dcbaed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14136b1021f15c70ca372310ce5bb440",
+    "url": "/static-reactjs-pocket-monsters/index.html"
+  },
+  {
+    "revision": "c498f75cd99211f3a99f",
+    "url": "/static-reactjs-pocket-monsters/static/css/main.70194629.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/static-reactjs-pocket-monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static-reactjs-pocket-monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c498f75cd99211f3a99f",
+    "url": "/static-reactjs-pocket-monsters/static/js/main.ce1e1855.chunk.js"
+  },
+  {
+    "revision": "57b4ca2094c9b7e0a3b5",
+    "url": "/static-reactjs-pocket-monsters/static/js/runtime-main.dc1dd9be.js"
+  }
+]);
